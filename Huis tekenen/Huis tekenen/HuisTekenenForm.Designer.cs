099c1f52cfc461c27ad566cad831b0a929bd94a2
Ruben H. <rhdesign.design@gmail.com>
2013-10-10 13:11:25 +0200

    Gebruik van Methodes

---
+++ Huis tekenen/Huis tekenen/HuisTekenenForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Huis_tekenen
+{
+    partial class HuisTekenenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTekenen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTekenen
+            // 
+            this.btnTekenen.Location = new System.Drawing.Point(12, 12);
+            this.btnTekenen.Name = "btnTekenen";
+            this.btnTekenen.Size = new System.Drawing.Size(75, 23);
+            this.btnTekenen.TabIndex = 0;
+            this.btnTekenen.Text = "Tekenen";
+            this.btnTekenen.UseVisualStyleBackColor = true;
+            this.btnTekenen.Click += new System.EventHandler(this.btnTekenen_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LemonChiffon;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(93, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // HuisTekenenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 227);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnTekenen);
+            this.Name = "HuisTekenenForm";
+            this.Text = "Huis tekenen";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTekenen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Huis tekenen/Huis tekenen/HuisTekenenForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Huis_tekenen
+{
+    public partial class HuisTekenenForm : Form
+    {
+        public HuisTekenenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTekenen_Click(object sender, EventArgs e)
+        {
+            Graphics paper;
+            paper = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+            TekenHuis(paper, pen, 10, 20, 70, 20);
+            TekenHuis(paper, pen, 10, 90, 50, 50);
+        }
+
+        private void TekenHuis(Graphics Tekengebied,
+                                Pen penToUse,
+                                int xPos,
+                                int yPos,
+                                int breedte,
+                                int hoogte)
+        {
+            TekenDak(Tekengebied, penToUse,
+                                    xPos, yPos, breedte, hoogte);
+            Tekengebied.DrawRectangle(penToUse, xPos, yPos + hoogte, breedte, hoogte); 
+        }
+
+        private void TekenDak(Graphics Tekengebied,
+                                Pen penToUse,
+                                int xPos,
+                                int yPos,
+                                int breedte,
+                                int hoogte)
+        {
+            Tekengebied.DrawLine(penToUse, xPos, yPos, xPos, yPos + hoogte);
+            Tekengebied.DrawLine(penToUse, xPos, yPos + hoogte, xPos + breedte, yPos + hoogte);
+            Tekengebied.DrawLine(penToUse, xPos + breedte, yPos + hoogte, xPos, yPos);
+        }
+    }
+}
