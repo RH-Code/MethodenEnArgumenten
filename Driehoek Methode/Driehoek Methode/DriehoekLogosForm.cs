179e13ce416142bed6fab632345fafbfa820389a
Ruben H. <rhdesign.design@gmail.com>
2013-10-10 10:48:13 +0200

    Driehoek + Logo Methode

---
+++ Driehoek Methode/Driehoek Methode/DriehoekLogosForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Driehoek_Methode
+{
+    public partial class DriehoekLogosForm : Form
+    {
+        public DriehoekLogosForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTeken_Click(object sender, EventArgs e)
+        {
+            Graphics paper;
+            paper = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+            TekenLogo(paper, pen, 10, 20);
+            TekenLogo(paper, pen, 100, 100);
+            TekenDriehoek(paper, pen, 100, 10, 40, 40);
+            TekenDriehoek(paper, pen, 10, 100, 20, 60);
+        }
+
+        private void TekenLogo(Graphics Tekengebied,
+                                Pen penToUse,
+                                int xPos,
+                                int yPos)
+        {
+            Tekengebied.DrawRectangle(penToUse, xPos, yPos, 20, 20);
+            Tekengebied.DrawRectangle(penToUse, xPos, yPos, 40, 40);
+            Tekengebied.DrawRectangle(penToUse, xPos, yPos, 60, 60);
+        }
+
+        private void TekenDriehoek(Graphics Tekengebied,
+                                    Pen penToUse,
+                                    int xPos,
+                                    int yPos,
+                                    int breedte,
+                                    int hoogte)
+        {
+            Tekengebied.DrawLine(penToUse, xPos, yPos, xPos, yPos + hoogte);
+            Tekengebied.DrawLine(penToUse, xPos, yPos, xPos + breedte, yPos + hoogte);
+            Tekengebied.DrawLine(penToUse, xPos, yPos + hoogte, xPos + breedte, yPos + hoogte);
+        }
+    }
+}
---
+++ LogoMethode/LogoMethode/LogoMethodeForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Logo_Methode
+{
+    public partial class LogoMethodeForm : Form
+    {
+        public LogoMethodeForm()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnTeken_Click(object sender, EventArgs e)
+        {
+            Graphics paper;
+            paper = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+
+        //Hier gaan we onze methode aanroepen:
+            TekenLogo(paper, pen, 10, 20);
+            TekenLogo(paper, pen, 100, 100);
+        }
+
+
+        //Hier maken we de methode aan:
+        //Methode header
+        private void TekenLogo(Graphics paper1,
+                                Pen pen1,
+                                int xPos,
+                                int yPos)
+        //einde methode header
+
+        //Methode body
+        {
+            paper1.DrawRectangle(pen1, xPos, yPos, 60, 60);
+            paper1.DrawRectangle(pen1, xPos, yPos, 40, 40);
+            paper1.DrawRectangle(pen1, xPos, yPos, 20, 20);
+        }
+        //einde methode body
+    }
+}
