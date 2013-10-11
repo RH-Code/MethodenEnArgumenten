f478574077efbfb3645b1828cd7dffae772b2a56
Ruben H. <rhdesign.design@gmail.com>
2013-10-11 13:34:39 +0200

    Oef. op Methode met referentie REF

---
+++ Pie Size/Pie Size/PieSizeForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Pie_Size
+{
+    partial class PieSizeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBereken = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBereken
+            // 
+            this.btnBereken.Location = new System.Drawing.Point(12, 133);
+            this.btnBereken.Name = "btnBereken";
+            this.btnBereken.Size = new System.Drawing.Size(200, 42);
+            this.btnBereken.TabIndex = 0;
+            this.btnBereken.Text = "Bereken";
+            this.btnBereken.UseVisualStyleBackColor = true;
+            this.btnBereken.Click += new System.EventHandler(this.btnBereken_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLength);
+            this.groupBox1.Controls.Add(this.txtWidth);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pie Size";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(76, 65);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 3;
+            this.txtLength.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(76, 25);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 2;
+            this.txtWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Length:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Width:";
+            // 
+            // PieSizeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 188);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnBereken);
+            this.Name = "PieSizeForm";
+            this.Text = "Pie size";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBereken;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Pie Size/Pie Size/PieSizeForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pie_Size
+{
+    public partial class PieSizeForm : Form
+    {
+        public PieSizeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBereken_Click(object sender, EventArgs e)
+        {
+            int pieWidth, pieLength;
+            pieWidth = Convert.ToInt32(txtWidth.Text);
+            pieLength = Convert.ToInt32(txtLength.Text);
+            IncreaseSize(ref pieWidth, ref pieLength);
+            IncreaseSize(ref pieWidth, ref pieLength);
+            IncreaseSize(ref pieWidth, ref pieLength);
+        }
+
+        private void IncreaseSize(ref int pieWidth, ref int pieLength)
+        {
+            int area;
+            pieWidth = pieWidth + 2;
+            pieLength = pieLength + 2;
+            area = pieWidth * pieLength;
+            string message = String.Format("Pie size: {0} by {1}. Area is {2}", pieWidth, pieLength, area);
+            MessageBox.Show(message);
+        }
+    }
+}
